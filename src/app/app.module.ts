ab21c48cf47083616c41b8a1958b49548db976ce
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-14 17:27:45 -0300

    feat: criado modulo de forma automatica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitoManualModule } from 'src/conceitos-manual/conceito-manual.module';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 
 // A partir daqui (imports: []), você pode importar outros módulos necessários para o seu aplicativo.
 @Module({
-  imports: [ConceitoManualModule],
+  imports: [ConceitoManualModule, ConceitosAutomaticoModule],
   controllers: [AppController],
   providers: [AppService],
 })
