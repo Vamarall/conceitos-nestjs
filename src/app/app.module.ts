a29b2c5b03cc88366641395adc46f14a04a51d51
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-14 17:23:42 -0300

    feat: criado modulo de forma manual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitoManualModule } from 'src/conceitos-manual/conceito-manual.module';
 
+// A partir daqui (imports: []), você pode importar outros módulos necessários para o seu aplicativo.
 @Module({
-  imports: [],
+  imports: [ConceitoManualModule],
   controllers: [AppController],
   providers: [AppService],
 })
