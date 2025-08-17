4d1eb8b1cada3adb7723bed9bddaa84d5d731b5b
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-17 18:31:52 -0300

    feat: seguranca - opcoes whitelist e forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Remove chaves que nao estao no DTO
+    forbidNonWhitelisted: true, // Retorna erro se chaves nao estao no DTO
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
