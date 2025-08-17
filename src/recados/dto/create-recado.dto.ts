6b5d3959ea499b798defd3fc3c545ad50aa0fcb6
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-17 18:08:05 -0300

    feat: validando dados de entrada com ValidationPipes

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, Min, MinLength } from "class-validator";
+
 export class CreateRecadoDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(100)
     readonly text: string;
+     
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(100)
     readonly de: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(100)
     readonly para: string;
 }
